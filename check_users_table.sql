c3f3e4dec3ad71a4d76f665e90cdfab32da5e979
David Deng <124044900+ddeng12@users.noreply.github.com>
2025-11-30 15:22:16 +0000

    Delete check_users_table.sql

--- check_users_table.sql
+++
@@ -1,7 +0,0 @@
--- Check the structure of your users table
--- Copy and paste this in phpMyAdmin to see what columns exist
-
-DESCRIBE users;
-
--- Also show any existing users
-SELECT * FROM users LIMIT 5;
